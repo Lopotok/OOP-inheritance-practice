813b298edb67defe5069a312dbb63b0f7d3f72d7
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-10 15:43:18 +0200

    Teljes feladat

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // Ezt nyugodtan módosítsd!
         Person person = new Person();
-        Staff staff = new Staff();
-        Student student = new Student();
+        Staff staff = new Staff("Fulop","Gyongyos", "Suli",123);
+        Student student = new Student("Tanulo", "Gyongyos", "Info", 2000, 12);
     }
 }
